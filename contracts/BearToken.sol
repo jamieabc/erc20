c708330d1b985d14eb6710e3f9a117a90b6622af
Aaron Chang <xesuschang@gmail.com>
2019-01-04 17:40:38 +0800

    add test contract

---
+++ contracts/BearToken.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.19;
+
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+/**
+ * @title BeatToken is a basic ERC20 Token
+ */
+contract BearToken is ERC20, Ownable{
+  uint256 public totalSupply;
+  string public name;
+  string public symbol;
+  uint32 public decimals;
+  mapping(address => uint256) public balances;
+
+  constructor() public {
+    symbol = "BEAR";
+    name = "BearToken";
+    decimals = 5;
+    totalSupply = 100000000000;
+
+    address owner = msg.sender;
+    balances[owner] = totalSupply;
+
+    emit Transfer(0x0, owner, totalSupply);
+  }
+}
